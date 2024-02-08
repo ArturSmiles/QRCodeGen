042c095fbd103856ecfe9b6ca369fc07b4116a35
ArturSmiles <arturkemmerer@gmx.net>
2024-02-08 18:27:37 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-img = qrcode.make(
-    input("What would you like to convert into a QR Code ?\n > "))
-name = input("What would you like to name the file ? \n > ")
-
-type(img)
-img.save(f"./GeneratedQrCodes/{name}.png")
